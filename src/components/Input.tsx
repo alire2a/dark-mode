77ca46fa315eb3cb2bd608c00211b61dcfb0caf1
alireza <alirezae973@gmail.com>
2021-11-21 16:39:35 +0330

    controlled input

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,12 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 
 type Props = {
   title: string;
   type: "text" | "email";
 };
 
-export const Input = ({ title,type }: Props) => {
+export const Input = ({ title, type }: Props) => {
+  const [value, setValue] = useState("");
   return (
-    <input type={type} placeholder={title} className="w-full	bg-white text-gray-500 border-gray-200 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 border rounded-sm h-10 p-3 my-2" />
+    <input
+      value={value}
+      onChange={e => setValue(e.target.value)}
+      type={type}
+      placeholder={title}
+      className="w-full	bg-white text-gray-500 border-gray-200 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 border rounded-sm h-10 p-3 my-2"
+    />
   );
 };
